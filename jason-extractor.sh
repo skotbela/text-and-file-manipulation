f6c9f0385f0e7a35d9a56e365e34a1e80b5dd2f3
skotbela <skotbela@gmail.com>
2021-04-28 16:43:57 +0200

    Phone numbers added

--- jason-extractor.sh
+++ jason-extractor.sh
@@ -1,1 +1,4 @@
-jq -r '(["First Name"] | (., map(length*"-"))), (.employees[] | [.firstName]) | @tsv' employees.json
+jq -r '(["Name"] | (., map(length*"-"))), (.employees[] | [.firstName]) | @tsv' employees.json
+echo " "
+jq -r '(["Name", "Phone Numbers"] | (., map(length*"-"))), (.employees[] | [.firstName, .phoneNumbers[].number]) | @tsv' employees.json
+
